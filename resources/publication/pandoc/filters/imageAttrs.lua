d4ccb16bf0691c8fc2b0cd025e4bae6696e791c6
Jeet Sukumaran <jeetsukumaran@gmail.com>
2024-02-22 19:50:06 -0800

    Update

--- resources/publication/pandoc/filters/imageAttrs.lua
+++ resources/publication/pandoc/filters/imageAttrs.lua
@@ -39,6 +39,47 @@ function xImage(elem)
 end
 
 
+-- function Image(elem)
+--     local captionStr = pandoc.utils.stringify(elem.caption)
+--     local parts = {}
+--     for part in string.gmatch(captionStr, "[^|]+") do
+--         table.insert(parts, string.match(part, "^%s*(.-)%s*$")) -- Trim whitespace
+--     end
+--     local width, height = nil, nil
+--     if #parts > 0 then
+--         width, height = parts[#parts]:match("^(%d+)%s*x?%s*(%d*)$")
+--         if width then
+--             table.remove(parts, #parts)
+--         end
+--     end
+--     if width then
+--         elem.attributes.width = width
+--         if height and height ~= "" then
+--             elem.attributes.height = height
+--         end
+--     end
+--     if #parts > 0 then
+--         local newCaption = table.concat(parts, " | ")
+--         elem.caption = pandoc.read(newCaption, 'markdown').blocks[1].content
+--     else
+--         elem.caption = {}
+--     end
+--     if FORMAT == "latex" or FORMAT == "pdf" or FORMAT == "beamer" then
+--         local rootPath = "/home/jeetsukumaran/site/storage/workspaces/note/projects/scribery/scribery.notes/"
+--         local absSrcPath = rootPath .. elem.src
+--         local latexString = "\\begin{figure}[htbp]\n\\centering\n"
+--         if (elem.attributes.width) then
+--             latexString = latexString .. "\\includegraphics[width=" .. elem.attributes.width .. "px]"
+--         end
+--         latexString = latexString .. "{" .. absSrcPath .. "}\n"
+--         latexString = latexString .. "\\caption{" .. pandoc.utils.stringify(elem.caption) .. "}\n"
+--         latexString = latexString .. "\\end{figure}"
+--         return pandoc.RawInline('latex', latexString)
+--     else
+--         return elem
+--     end
+-- end
+
 function Image(elem)
     local captionStr = pandoc.utils.stringify(elem.caption)
     local parts = {}
@@ -53,9 +94,9 @@ function Image(elem)
         end
     end
     if width then
-        elem.attributes.width = width
+        elem.attributes.width = width .. "px"
         if height and height ~= "" then
-            elem.attributes.height = height
+            elem.attributes.height = height .. "px"
         end
     end
     if #parts > 0 then
@@ -64,25 +105,29 @@ function Image(elem)
     else
         elem.caption = {}
     end
+
     if FORMAT == "latex" or FORMAT == "pdf" or FORMAT == "beamer" then
-        local rootPath = "/home/jeetsukumaran/site/storage/workspaces/note/projects/scribery/scribery.notes/"
-        local absSrcPath = rootPath .. elem.src
+        -- Dynamically get the first resource path or use a default if not set
+        local rootPath = PANDOC_STATE.resource_path[1] or "."
+        local absSrcPath = rootPath .. "/" .. elem.src
         local latexString = "\\begin{figure}[htbp]\n\\centering\n"
-        if (elem.attributes.width) then
-            latexString = latexString .. "\\includegraphics[width=" .. elem.attributes.width .. "px]"
+        if elem.attributes.width then
+            -- Convert width attribute to LaTeX compatible format if specified
+            latexString = latexString .. "\\includegraphics[width=" .. elem.attributes.width .. "]"
+        else
+            latexString = latexString .. "\\includegraphics"
         end
         latexString = latexString .. "{" .. absSrcPath .. "}\n"
+        -- latexString = latexString .. "\\caption{" .. pandoc.utils.stringify(elem.caption) .. "}\n"
+        -- latexString = latexString .. "\\end{figure}"
         latexString = latexString .. "\\caption{" .. pandoc.utils.stringify(elem.caption) .. "}\n"
         latexString = latexString .. "\\end{figure}"
+
+        -- Return the raw LaTeX block
+        -- return pandoc.RawBlock('latex', latexString)
         return pandoc.RawInline('latex', latexString)
     else
         return elem
     end
 end
-
-return {
-    {
-        Image = Image,
-    }
-}
 
