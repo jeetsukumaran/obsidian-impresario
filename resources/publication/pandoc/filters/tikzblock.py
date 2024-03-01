bb25ccfd0ca53604a17017952ec291093240c5cb
Jeet Sukumaran <jeetsukumaran@gmail.com>
2024-02-29 20:59:10 -0800

    Update

--- resources/publication/pandoc/filters/tikzblock.py
+++ resources/publication/pandoc/filters/tikzblock.py
@@ -5,15 +5,25 @@ from pandocfilters import toJSONFilter, RawBlock
 import sys
 import re
 
+begin_doc_pattern = re.compile(
+        r"^.*?\\begin\s*{\s*document\s*}",
+        re.DOTALL,
+)
+end_doc_pattern = re.compile(
+    r"\\end\s*{\s*document\s*}",
+    re.DOTALL,
+)
+
 def tikz_to_latex_advanced(key, value, format, meta):
     if key == 'CodeBlock':
         [[ident, classes, kvs], code] = value
         if 'tikz' in classes:
-            begin_doc_pattern = r"\\begin\s*{\s*document\s*}"
-            end_doc_pattern = r"\\end\s*{\s*document\s*}"
-            tikz_code = re.sub(f"{begin_doc_pattern}.*", "", code, 1)
-            tikz_code = re.sub(f".*{end_doc_pattern}", "", tikz_code, 1)
-            # sys.stderr.write(tikz_code + "\n")
+            tikz_code = code
+            # sys.stderr.write(f"\n>>>\n{tikz_code}\n<<<")
+            tikz_code = begin_doc_pattern.sub("", tikz_code, 1)
+            # sys.stderr.write(f"\n>>>\n{tikz_code}\n<<<")
+            tikz_code = end_doc_pattern.sub("", tikz_code, 1)
+            # sys.stderr.write(f"\n>>>\n{tikz_code}\n<<<")
             latex = '\\begin{tikzpicture}[domain=0:4]\n' + tikz_code + '\n\\end{tikzpicture}'
             return RawBlock('latex', latex)
 
